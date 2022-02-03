aea0ecfa06cf2e77e417147f01b122cb01bdef43
PFSV <96779838+PFSV@users.noreply.github.com>
2022-02-03 09:18:35 +0900

    Add files via upload

---
+++ 2751 pypy.py
@@ -0,0 +1,21 @@
+
+#string, list index
+
+llist = [1,2,3,4,5]
+string = '12345'
+
+print(llist[0]) #result 1
+print(string[0]) #result 1
+
+# object in list,string
+
+print(1 in llist) #result = True
+print("1" in string) #result = True
+
+#index in list,string
+
+print(llist.index(3)) #result = 2
+print(string.index("3")) #result = 2 
+
+
+
